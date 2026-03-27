fb869584734d65ffe9ce0e75bf608ae316a48ddd
yooseungyeon31 <ysy8317433@gmail.com>
2026-03-27 14:43:31 +0900

    숫자 정렬하기 버블정렬

--- main.cpp
+++ main.cpp
@@ -7,48 +7,51 @@
 #include "Monster.h"
 
 #include <fstream>
+#include <algorithm>
 
 using namespace std;
 
-class Singleton
-{
-private:
-	Singleton()
-	{
 
-	}
-
-	static Singleton* Instance;
-public:
-	static Singleton* GetInstance()
-	{
-		if (Instance == nullptr)
-		{
-			Instance = new Singleton();
-		}
-		return Instance;
-	}
-};
-
-Singleton* Singleton::Instance = nullptr;
+////비교 함수 
+//bool compare(int a, int b)
+//{
+//	return a < b;
+//}
 
 int main()
 {
+	int a[10] = { 9,1,3,5,4,6,7,8,2,10 };
+	//search and sort
+	//selection and bubble
+	//첫번째 찍고 다음 숫자랑 비교해서 작으면 바꾸셈
+
+	//bool compare (int a, int b);
 
 
-	//UEngine* MyEngine = new UEngine();
-	//MyEngine->GetWorld()->Load("level01.umap");
+	//sort(a, a+10, compare);
 
-	//MyEngine->Run();
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	
+	//	cout << a[i] << " ";
 
-	//delete MyEngine;
+	//}
 
-	GEngine->Init();
+	//-----------------------------
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = i + 1; j < 10; j++)
+		{
+			if (a[i] > a[j])
+			{
+				int Temp = a[i];
+				a[i] = a[j];
+				a[j] = Temp;
+			}
+		}
 
-	GEngine->GetWorld()->Load("level01.umap");
-
-	GEngine->Run();
-
+		cout << a[i] << " ";
+	}
 
 	return 0;
-}
+} 
