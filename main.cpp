196a2463834d838a4d3e78850b9d5c64199002fa
yooseungyeon31 <ysy8317433@gmail.com>
2026-03-27 15:26:21 +0900

    람다 함수, 함수포인터

--- main.cpp
+++ main.cpp
@@ -11,36 +11,11 @@
 
 using namespace std;
 
-
-////비교 함수 
-//bool compare(int a, int b)
-//{
-//	return a < b;
-//}
-
-int main()
+void Sort(int* a, int InLength)
 {
-	int a[10] = { 9,1,3,5,4,6,7,8,2,10 };
-	//search and sort
-	//selection and bubble
-	//첫번째 찍고 다음 숫자랑 비교해서 작으면 바꾸셈
-
-	//bool compare (int a, int b);
-
-
-	//sort(a, a+10, compare);
-
-	//for (int i = 0; i < 10; i++)
-	//{
-	//	
-	//	cout << a[i] << " ";
-
-	//}
-
-	//-----------------------------
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < InLength; i++)
 	{
-		for (int j = i + 1; j < 10; j++)
+		for (int j = i + 1; j < InLength; j++)
 		{
 			if (a[i] > a[j])
 			{
@@ -52,6 +27,41 @@ int main()
 
 		cout << a[i] << " ";
 	}
+}
+
+
+
+
+int main()
+{
+	int a[10] = { 9,1,3,5,4,6,7,8,2,10 };
+	//search and sort
+	//selection and bubble
+	//첫번째 찍고 다음 숫자랑 비교해서 작으면 바꾸셈
+
+	Sort(a, 10);
+
+	//메인안에 넣은 버전
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	for (int j = i + 1; j < 10; j++)
+	//	{
+	//		if (a[i] > a[j])
+	//		{
+	//			int Temp = a[i];
+	//			a[i] = a[j];
+	//			a[j] = Temp;
+	//		}
+	//	}
+
+	//	cout << a[i] << " ";
+	//}
+	//-------------------------------
+
+
+	//-----------------------------
+
 
 	return 0;
 } 
+
