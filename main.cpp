90c531a0961a46fe6fc69ed1f6eb27bf780a0cae
yooseungyeon31 <ysy8317433@gmail.com>
2026-04-01 10:18:47 +0900

    SDL2 라이브러리 설치 연결

--- main.cpp
+++ main.cpp
@@ -2,24 +2,39 @@
 #include "Engine.h"
 #include "World.h"
 
-#include "Actor.h"
-#include "Player.h"
-#include "Monster.h"
+//#include "Actor.h"
+//#include "Player.h"
+//#include "Monster.h"
+//
+//#include <fstream>
+//#include <algorithm>
+#include "SDL.h"
+//#include "SDL_main.h"
 
-#include <fstream>
-#include <algorithm>
+//사용할 라이브러리 복사하고 
+// include 경로 추가, lib 경로 추가
+// library 파일 등록 
+
+//사용할 라이브러리 파일 추가
+#pragma comment(lib,"SDL2")
+#pragma comment(lib,"SDL2main")
 
 using namespace std;
-
-int main()
+//-----------------------------------------
+int SDL_main(int argc, char* argv)
 {
-	GEngine->GetWorld()->Load("level01.umap");
-
-	GEngine->Run();
-
-	delete GEngine;
-
-
 	return 0;
-} 
+}
+
+//int main()
+//{
+//	GEngine->GetWorld()->Load("level01.umap");
+//
+//	GEngine->Run();
+//
+//	delete GEngine;
+//
+//
+//	return 0;
+//} 
 
