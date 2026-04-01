77fc6f4ffadbba19a879c321986f12fb58f968e0
yooseungyeon31 <ysy8317433@gmail.com>
2026-04-01 11:17:42 +0900

    렌더러 사용하기

--- main.cpp
+++ main.cpp
@@ -2,12 +2,7 @@
 #include "Engine.h"
 #include "World.h"
 
-//#include "Actor.h"
-//#include "Player.h"
-//#include "Monster.h"
-//
-//#include <fstream>
-//#include <algorithm>
+
 #include "SDL.h"
 //#include "SDL_main.h"
 
@@ -21,8 +16,48 @@
 
 using namespace std;
 //-----------------------------------------
-int SDL_main(int argc, char* argv)
+int SDL_main(int argc, char* argv[])
 {
+	SDL_Init(SDL_INIT_EVERYTHING); //윈도우 초기화
+
+	//윈도우 가리키고 만들고
+	SDL_Window* MyWindow = SDL_CreateWindow("Hello",100,100,640,480, SDL_WINDOW_SHOWN);
+
+	//GPU, 붓
+	SDL_Renderer* MyRender = SDL_CreateRenderer(MyWindow, -1, 0);
+
+	
+	//계속 실행
+	while (true)
+	{
+		SDL_Event MyEvent;
+		SDL_PollEvent(&MyEvent); //계속 물어보는거
+
+		//이벤트 타임이 나가기면 나가기
+		if (MyEvent.type == SDL_QUIT)
+		{
+			break;
+		}
+
+		//GPU한테 보낼 명령어 모음 
+		//이거하고 이거해라 적은것
+		SDL_SetRenderDrawColor(MyRender, 255, 255, 255, 255);
+		SDL_RenderClear(MyRender);
+		
+
+		//이제 보내기
+		SDL_RenderPresent(MyRender);
+	
+	}
+	//랜더 한거 삭제
+	SDL_DestroyRenderer(MyRender);
+	//윈도우 삭제
+	SDL_DestroyWindow(MyWindow);
+
+	
+
+	SDL_Quit();
+
 	return 0;
 }
 
