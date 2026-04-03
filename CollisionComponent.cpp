e7319bed581f111a3561723efe2444e8cb95e02b
yooseungyeon31 <ysy8317433@gmail.com>
2026-04-03 17:49:10 +0900

    주석으로 필기할거함

--- CollisionComponent.cpp
+++ CollisionComponent.cpp
@@ -30,19 +30,25 @@ void UCollisionComponent::Tick()
 
 		for (auto OtherComponent : Other->Components)
 		{
+			//상대방 엑터가 가진 여러 충돌중에서 충돌 컴포넌트를 가지고 있는 것만 골라낸다.
 			UCollisionComponent* OtherCollision = dynamic_cast<UCollisionComponent*>(OtherComponent);
 			if (OtherCollision)
 			{
+				//좌표 기반 충돌 검사. 나와 상대방의 좌표가 일치하는 순간을 충돌로간주
+				//조건 - 히트가 켜져있고 좌표가 같을 때
 				if (OtherCollision->bIsGenerateHit &&
 					Owner->GetX() == Other->GetX() && Owner->GetY() == Other->GetY())
 				{
+					//리시브히트 함수 실행
 					Other->ReceiveHit(Owner);
 					Owner->ReceiveHit(Other);
 				}
-
+				
+				//오버랩이 켜져있고 좌표가 같을 때
 				if (OtherCollision->bIsGenerateOverlap &&
 					Owner->GetX() == Other->GetX() && Owner->GetY() == Other->GetY())
 				{
+					//델리게이트(함수 포인터를 실행함) 실행. 
 					if (Owner->OnActorBeginOverlap)
 					{
 						Owner->OnActorBeginOverlap(Other);
@@ -56,3 +62,6 @@ void UCollisionComponent::Tick()
 		}
 	}
 }
+
+//dynamic_cast의 의미: 상대방 엑터가 가진 여러 부품 중에서 충돌 컴포넌트 타입인 것만 골라내는 안전한 형변환
+//충돌 컴퍼넌트가 없다면 아더컬리션은 널이되어 아래 로직을 건너 뒴. 
